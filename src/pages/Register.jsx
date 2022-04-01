916dd5d02cf544d4979e9ff54ec025cae42eb7f4
DarielOWL <darieldesosa@gmail.com>
2022-04-01 18:34:10 -0300

    Vista Register Fixed

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,9 @@
-import { useState, useRef } from "react";
+import React, { useState } from "react";
+
+//React Router
+import { Link } from "react-router-dom";
+
+//Css React
 import {
   Container,
   Row,
@@ -9,98 +14,60 @@ import {
   Image,
   InputGroup,
 } from "react-bootstrap";
-import { Link, useHistory } from "react-router-dom";
 
-import logo from "./../logo.svg";
+import logo from '../assets/img/logoF.png'
+
+//Dependencias de Firebase
+import firebaseApp from "../firebase/credencial";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { getFirestore, doc, collection, setDoc } from "firebase/firestore";
+const auth = getAuth(firebaseApp);
 
 function Register() {
-  // Component states
-  const history = useHistory();
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
-  const [errors, setErrors] = useState({});
+  const firestore = getFirestore(firebaseApp);
+  const [isRegistrando, setIsRegistrando] = useState(false);
 
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const confirmPasswordRef = useRef();
-
-  const handleFormData = (e) => {
-    e.preventDefault();
-    setField(e.target.name, e.target.value);
-  };
-
-  const setField = (field, value) => {
-    setFormData({
-      ...formData,
-      [field]: value,
+  async function registrarUsuario(email, password, rol) {
+    const infoUsuario = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    ).then((usuarioFirebase) => {
+      return usuarioFirebase;
     });
-  };
 
-  const findFormErrors = () => {
-    const { email, password, confirmPassword } = formData;
-    const newErrors = {};
-    console.log(email.indexOf("@"));
-    // email
-    if (!email || email === "") newErrors.email = "Email must not be blank";
-    else if (email.indexOf("@") === -1 || email.indexOf(".") === -1)
-      newErrors.email = "Please enter a valid email";
+    console.log(infoUsuario.user.uid);
+    const docuRef = doc(firestore, `usuarios/${infoUsuario.user.uid}`);
+    setDoc(docuRef, { correo: email, rol: rol });
+  }
 
-    // password errors
-    if (!password.trim() || password.trim().length < 8)
-      newErrors.password = "Password must be 8 characters or more!";
-
-    // Confirm password errors
-    if (password !== confirmPassword)
-      newErrors.confirmPassword = "Passwords do not match!";
-
-    return newErrors;
-  };
-
-  const registerUser = (e) => {
+  function submitHandler(e) {
     e.preventDefault();
 
-    // geterrors
-    const newErrors = findFormErrors();
+    const email = e.target.elements.email.value;
+    const password = e.target.elements.password.value;
+    const rol = e.target.elements.rol.value;
 
-    // Conditional logic:
-    if (Object.keys(newErrors).length > 0) {
-      // We got errors!
-      setErrors(newErrors);
+    console.log("submit", email, password, rol);
+
+    if(password.length<6){
+      window.alert("nop");
       return false;
     }
 
-    // create new user object and send it to api
-    const newuser = {
-      email: formData.email,
-      password: formData.password,
-    };
+    if (isRegistrando) {
+      // registrar
+      registrarUsuario(email, password, rol);
+    } else {
+      // login
+      signInWithEmailAndPassword(auth, email, password);
+    }
+  }
 
-    fetch(`https://user-manager-three.vercel.app/api/user/register`, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(newuser),
-    })
-      .then((res) => res.json())
-      .then((result) => {
-        if (result.error) {
-          alert(result.message);
-          return true;
-        }
-        setFormData({});
-        setErrors({});
-        alert("Registration successful");
-        history.push("/login");
-      })
-      .catch((err) => {
-        console.log("this error occurred", err);
-        alert(err.message);
-      });
-  };
   return (
     <Container>
       <Row
@@ -131,65 +98,37 @@ function Register() {
 
             {/* Form here */}
             <Toast.Body>
-              <Form noValidate onSubmit={registerUser}>
+              <Form>
                 <Form.Row>
+                  
                   {/* Email */}
                   <Form.Group as={Col} sm={12}>
                     <Form.Label>Email</Form.Label>
-                    <InputGroup hasValidation>
+                    <InputGroup>
                       <Form.Control
                         type="email"
                         required
-                        isInvalid={!!errors.email}
                         placeholder="Enter email"
                         name="email"
                         id="email"
-                        ref={emailRef}
-                        onBlur={handleFormData}
+                        onBlur={submitHandler}
                       />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.email}
-                      </Form.Control.Feedback>
                     </InputGroup>
                   </Form.Group>
                 </Form.Row>
 
-                {/* Password row */}
+                {/* Password */}
                 <Form.Row>
                   <Form.Group as={Col} md={6} sm={12}>
                     <Form.Label>Password</Form.Label>
-                    <InputGroup hasValidation>
+                    <InputGroup>
                       <Form.Control
                         type="password"
                         placeholder="Password"
                         name="password"
                         id="password"
-                        required
-                        isInvalid={!!errors.password}
-                        ref={passwordRef}
-                        onBlur={handleFormData}
+                        onBlur={submitHandler}
                       />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.password}
-                      </Form.Control.Feedback>
-                    </InputGroup>
-                  </Form.Group>
-                  <Form.Group as={Col} md={6} sm={12}>
-                    <Form.Label>Confirm Password</Form.Label>
-                    <InputGroup hasValidation>
-                      <Form.Control
-                        type="password"
-                        placeholder="Confirm Password"
-                        name="confirmPassword"
-                        id="confirmPassword"
-                        required
-                        isInvalid={!!errors.confirmPassword}
-                        ref={confirmPasswordRef}
-                        onBlur={handleFormData}
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.confirmPassword}
-                      </Form.Control.Feedback>
                     </InputGroup>
                   </Form.Group>
                 </Form.Row>
