a47813b6508f8fa9b0add0482b4bb43be0436811
DarielOWL <darieldesosa@gmail.com>
2022-04-01 21:04:46 -0300

    Inicio de Sesion Codificado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,45 +8,51 @@ import {
   Image,
 } from "react-bootstrap";
 
-import { useContext } from "react";
-import { Link, useHistory } from "react-router-dom";
+
+import { Link } from "react-router-dom";
 
 import logo from "../assets/img/logoF.png";
-import { useForm } from "react-hook-form";
-import { AppContext } from "../components/StateProvider";
+
+//Dependencias de Firebase
+import firebaseApp from "../firebase/credencial";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { getFirestore, doc, collection, setDoc } from "firebase/firestore";
+const auth = getAuth(firebaseApp);
 
 function Login() {
-  const history = useHistory();
-  const { register, handleSubmit } = useForm();
-  const { setState, handleSetCookie } = useContext(AppContext);
 
-  const login = ({ email, password }) => {
-    fetch(`https://user-manager-three.vercel.app/api/user/login`, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
+
+  //const firestore = getFirestore(firebaseApp);
+
+  //Loging con Firebase
+  function submitHandlerLogin(e) {
+    
+    const email = e.target.elements.email.value;
+    const password = e.target.elements.password.value;
+    //const rol = e.target.elements.rol.value;
+
+    signInWithEmailAndPassword(auth, email, password)  
+     .then((userCredential) => {
+      // Iniciar Sesion
+      const user = userCredential.user;
+      let email = user.email;
+      
+      let usuario ={
+        email: user.email,
+        usuario: email.split("@")[0],
+      }
     })
-      .then((res) => res.json())
-      .then((result) => {
-        if (result.error) {
-          return alert(result.message);
-        }
-        handleSetCookie(result.body);
-        setState((prevstate) => {
-          return {
-            ...prevstate,
-            isLoggedIn: true,
-          };
-        });
-        history.replace("/dashboard");
-      })
-      .catch((err) => {
-        console.log("this error occurred", err);
-        return alert(err.message);
-      });
-  };
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      //window.alert("Error : " + errorMessage);
+    });
+  }
+
 
   return (
     <Container>
@@ -68,7 +74,7 @@ function Login() {
                     className="fas fa-sign-in-alt icon"
                     arial-hidden="true"
                   ></i>
-                  <strong>Login</strong>
+                  <strong>Inicio Sesion</strong>
                 </h2>
               </Col>
               <Col sm={4}>
@@ -78,27 +84,28 @@ function Login() {
               </Col>
             </Row>
             <hr />
+            
             <Toast.Body>
-              <Form onSubmit={handleSubmit(login)}>
+              <Form onSubmit={submitHandlerLogin}>
                 <Form.Group>
+                  {/* Email */}
                   <Form.Label>Email</Form.Label>
                   <Form.Control
                     type="email"
                     placeholder="Enter email"
                     name="email"
                     id="email"
-                    {...register("email", { required: true })}
                   />
                 </Form.Group>
 
                 <Form.Group>
+                  {/* Email */}
                   <Form.Label>Password</Form.Label>
                   <Form.Control
                     type="password"
                     placeholder="Password"
                     name="password"
                     id="password"
-                    {...register("password", { required: true })}
                   />
                 </Form.Group>
 
@@ -108,7 +115,7 @@ function Login() {
               </Form>
 
               <p>
-                Not yet registered? <Link to="/register">Click here</Link>
+                ¿Aun no estas Registrado? <Link to="/register">Click Aqui</Link>
               </p>
             </Toast.Body>
           </Toast>
